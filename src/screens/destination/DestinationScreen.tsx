c9a2183418676d4cb5c52dfb9175479a843cdc6f
Mohammad Azimi <mohammadazimi91@gmail.com>
2023-03-04 07:05:25 +0330

    added default coords and center pin mark

--- src/screens/destination/DestinationScreen.tsx
+++ src/screens/destination/DestinationScreen.tsx
@@ -1,6 +1,7 @@
-import React, {FC} from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {FC, useState} from 'react';
+import {View, StyleSheet, Text, Pressable} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {HomeStackParamListType} from '../../navigation/RouteParamsType';
 
 import MapboxGL from '@rnmapbox/maps';
@@ -15,11 +16,50 @@ type NavigationProps = NativeStackScreenProps<
   'DestinationScreen'
 >;
 
-const DestinationScreen: FC<NavigationProps> = ({}) => {
+const PIN_SIZE = 32;
+const INITIAL_COORDS = [51.3857, 35.7019];
+
+const DestinationScreen: FC<NavigationProps> = ({navigation}) => {
+  const [layout, setLayout] = useState<{
+    width: number | null;
+    height: number | null;
+  }>({width: null, height: null});
   return (
     <View style={styles.container}>
-      <View style={styles.mapContainer}>
-        <MapboxGL.MapView style={styles.map} />
+      <View
+        style={styles.mapContainer}
+        onLayout={e =>
+          setLayout({
+            width: e.nativeEvent.layout.width,
+            height: e.nativeEvent.layout.height,
+          })
+        }>
+        <MapboxGL.MapView style={styles.map}>
+          <MapboxGL.Camera zoomLevel={11} centerCoordinate={INITIAL_COORDS} />
+        </MapboxGL.MapView>
+        {!!layout.width && !!layout.height && (
+          <View
+            style={[
+              styles.pinContainer,
+              {
+                transform: [
+                  {translateX: layout.width / 2 - PIN_SIZE / 2},
+                  {translateY: layout.height / 2 - PIN_SIZE},
+                ],
+              },
+            ]}>
+            <Icon name={'pin'} color={'red'} size={PIN_SIZE} />
+          </View>
+        )}
+      </View>
+      <View style={styles.bottomContainer}>
+        <Text>{'Select your delivery location to submit your order'}</Text>
+        <Pressable
+          style={styles.buttonContainer}
+          // @ts-ignore
+          onPress={() => navigation.navigate('OrderScreen')}>
+          <Text style={styles.buttonText}>{'Submit'}</Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -35,5 +75,41 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1,
+  },
+  pinContainer: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: PIN_SIZE,
+    height: PIN_SIZE,
+    // top: '50%',
+    // alignSelf: 'center',
+    // backgroundColor: 'yellow',
+  },
+  bottomContainer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: 130,
+    backgroundColor: 'white',
+    elevation: 5,
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    justifyContent: 'space-between',
+    borderTopRightRadius: 12,
+    borderTopLeftRadius: 12,
+  },
+  buttonContainer: {
+    width: '100%',
+    height: 48,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'dodgerblue',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '700',
   },
 });
